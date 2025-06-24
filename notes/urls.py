4eb73fcff24657bad912523c7e41f51374f73c1c
szweijin <128682956+szweijin@users.noreply.github.com>
2025-06-24 10:14:54 +0800

    Update urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -7,14 +7,14 @@ from .views import (
 
 urlpatterns = [
     # --- 前端頁面 URL ---
-    path('', home_view, name='home'), # 首頁
+    path('', home_view, name='home'),                  # 首頁
     path('register/', register_view, name='register'), # 註冊頁面
-    path('login/', login_view, name='login'), # 登入頁面
-    path('logout/', logout_view, name='logout'), # 登出動作
+    path('login/', login_view, name='login'),          # 登入頁面
+    path('logout/', logout_view, name='logout'),       # 登出動作
 
-    path('notes_web/', note_list_view, name='note_list'), # 筆記列表 (Web 版)
-    path('notes_web/create/', note_create_view, name='note_create'), # 創建筆記 (Web 版)
-    path('notes_web/<int:pk>/edit/', note_update_view, name='note_update'), # 編輯筆記 (Web 版)
+    path('notes_web/', note_list_view, name='note_list'),                     # 筆記列表 (Web 版)
+    path('notes_web/create/', note_create_view, name='note_create'),          # 創建筆記 (Web 版)
+    path('notes_web/<int:pk>/edit/', note_update_view, name='note_update'),   # 編輯筆記 (Web 版)
     path('notes_web/<int:pk>/delete/', note_delete_view, name='note_delete'), # 刪除筆記 (Web 版)
 
 
