ca8f78f8d355319ba180b09314050bfff90a8c95
szweijin <128682956+szweijin@users.noreply.github.com>
2025-06-24 10:14:13 +0800

    Update serializers.py

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Note # 從同一個目錄下的 models.py 導入 Note 模型
+from .models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Note # 指定這個序列化器對應的 Django 模型是 Note
-        # fields = '__all__' # 包含模型中所有的欄位
-        fields = ['id', 'title', 'content', 'created_at', 'user'] # 明確列出要包含的欄位
-        read_only_fields = ['user', 'created_at'] # 將 user 和 created_at 設定為只讀
+        model = Note                                                # 指定這個序列化器對應的 Django 模型是 Note
+        # fields = '__all__'                                        # 包含模型中所有的欄位
+        fields = ['id', 'title', 'content', 'created_at', 'user']   # 明確列出要包含的欄位
+        read_only_fields = ['user', 'created_at']                   # 將 user 和 created_at 設定為唯讀
